7278864ab50a6795d09588e82afd5283692d8ca2
David Nebauer <david@nebauer.org>
2022-06-14 17:08:28 +0930

    update copyright date

--- vim/.vim/templates/template.pl
+++ vim/.vim/templates/template.pl
@@ -319,7 +319,7 @@ ${author}
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2017 ${author}
+Copyright (c) 2022 ${author}
 
 This script is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
