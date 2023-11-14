454952c6dd0663bb5081d316d5a4727f48e8d41b
David Nebauer <david@nebauer.org>
2023-11-14 22:39:18 +0930

    unsuccessful attempt to use dressing for vim.ui

--- vim/.config/nvim/lua/plugins/noice-nvim.lua
+++ vim/.config/nvim/lua/plugins/noice-nvim.lua
@@ -1,6 +1,15 @@
 --[[ folke/noice.nvim : replaces the UI for messages, cmdline and the popupmenu ]]
+
 return {
   "folke/noice.nvim",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
   opts = {
     cmdline = {
       enabled = true, -- enables the Noice cmdline UI
@@ -10,7 +19,7 @@ return {
       -- NOTE: If you enable messages, then the cmdline is enabled automatically.
       -- This is a current Neovim limitation.
       enabled = true, -- enables the Noice messages UI
-      view = "mini", -- default view for messages
+      view = "notify", -- default view for messages
       view_error = "mini", -- view for errors
       view_warn = "mini", -- view for warnings
       view_history = "messages", -- view for :messages
