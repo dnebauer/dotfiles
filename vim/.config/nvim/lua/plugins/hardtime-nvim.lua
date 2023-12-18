b5b5d4e27f8b625b2470e6852701b52d7ef53bea
David Nebauer <david@nebauer.org>
2023-12-18 14:13:23 +0930

    add hardtime.nvim plugin

---
+++ vim/.config/nvim/lua/plugins/hardtime-nvim.lua
@@ -0,0 +1,9 @@
+--[[ m4xshen/hardtime.nvim : establish good command workflow and habit ]]
+
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+}
