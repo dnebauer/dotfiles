0560630add022d072176124622ee1340aa023ab6
David Nebauer <david@nebauer.org>
2024-01-31 23:20:01 +0930

    add and set spaceship theme

--- zsh/.config/zshrc.d/70-oh-my-zsh.zsh
+++ zsh/.config/zshrc.d/70-oh-my-zsh.zsh
@@ -5,7 +5,8 @@
 #ZSH_THEME='robbyrussell'
 #ZSH_THEME='juanghurtado'
 #ZSH_THEME='josh'
-ZSH_THEME='miloshadzic'
+#ZSH_THEME='miloshadzic'
+ZSH_THEME='spaceship'
 
 # setup
 source $ZSH/oh-my-zsh.sh
