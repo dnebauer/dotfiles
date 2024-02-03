c44ea4dcc56b8c638bc0252e2205ec458aba1963
David Nebauer <david@nebauer.org>
2024-02-03 11:01:52 +0930

    add toggleterm plugin

---
+++ vim/.config/nvim/lua/plugins/toggleterm-nvim.lua
@@ -0,0 +1,22 @@
+--[[ akinsho/toggleterm.nvim : persist and toggle multiple terminals ]]
+
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return math.floor(vim.opt.lines:get() * 0.5)
+      elseif term.direction == "vertical" then
+        return math.floor(vim.opt.columns:get() * 0.4)
+      end
+    end,
+  },
+  keys = {
+    {
+      "<Leader>td",
+      "<Cmd>ToggleTerm direction=horizontal<CR>",
+      desc = "Open a horizontal terminal in the current directory",
+    },
+  },
+}
