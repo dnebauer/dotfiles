78d825e196c8704e530d45b84dc7b55905710847
David Nebauer <david@nebauer.org>
2024-02-08 20:42:06 +0930

    install plugin nvim-macros

---
+++ vim/.config/nvim/lua/plugins/nvim-macros.lua
@@ -0,0 +1,14 @@
+--[[ kr40/nvim-macros : macro management ]]
+
+return {
+  "kr40/nvim-macros",
+  cmd = { "MacroSave", "MacroYank", "MacroSelect", "MacroDelete" },
+  opts = {
+    -- json_file_path: location where the macros will be stored
+    json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/macros.json"),
+    -- default_macro_register: use as default register for :Macro{Yank,Save,Select} Raw functions
+    default_macro_register = "q",
+    -- json_formatter: can be "none"|"jq"|"yq" used to pretty print the json file (choice must be installed!)
+    json_formatter = "none",
+  },
+}
