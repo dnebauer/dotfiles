ea8b355b1d85b0f3561c136a4ca9e502c10ba179
David Nebauer <david@nebauer.org>
2024-02-08 23:30:42 +0930

    change location of macros file

--- vim/.config/nvim/lua/plugins/nvim-macros.lua
+++ vim/.config/nvim/lua/plugins/nvim-macros.lua
@@ -5,7 +5,7 @@ return {
   cmd = { "MacroSave", "MacroYank", "MacroSelect", "MacroDelete" },
   opts = {
     -- json_file_path: location where the macros will be stored
-    json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/macros.json"),
+    json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/misc/kr40_nvim-macros/macros.json"),
     -- default_macro_register: use as default register for :Macro{Yank,Save,Select} Raw functions
     default_macro_register = "q",
     -- json_formatter: can be "none"|"jq"|"yq" used to pretty print the json file (choice must be installed!)
