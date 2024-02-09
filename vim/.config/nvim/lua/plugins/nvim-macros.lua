0216ae30aec0d283309b164a720d6602bd59b4e1
David Nebauer <david@nebauer.org>
2024-02-09 21:08:00 +0930

    use custom save file

--- vim/.config/nvim/lua/plugins/nvim-macros.lua
+++ vim/.config/nvim/lua/plugins/nvim-macros.lua
@@ -6,9 +6,10 @@ return {
   opts = {
     -- json_file_path: location where the macros will be stored
     json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/misc/kr40_nvim-macros/macros.json"),
+    --json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/macros.json"),
     -- default_macro_register: use as default register for :Macro{Yank,Save,Select} Raw functions
     default_macro_register = "q",
     -- json_formatter: can be "none"|"jq"|"yq" used to pretty print the json file (choice must be installed!)
-    json_formatter = "none",
+    json_formatter = "jq",
   },
 }
