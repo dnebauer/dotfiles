4a370121c4263ce1bf33cb15ce962962f162486e
David Nebauer <david@nebauer.org>
2024-02-26 23:42:25 +0930

    switch to bash script that converts with pandoc

--- qute/.local/share/qutebrowser/userscripts/SaveMarkdown.sh
+++ qute/.local/share/qutebrowser/userscripts/SaveMarkdown.sh
@@ -1,125 +1,206 @@
 #!/usr/bin/env bash
+
 # Qutebrowser userscript that converts the current page to markdown and saves
 # it to a filepath specified by the user.
 #
 # Suggested keybinding (for "save markdown"):
 # spawn --userscript SaveMarkdown.sh
 #     sm
-#
-# Requirements:
-#  - assumed to be available (not tested for): which
-#  - tested for: basename, cp, html2text, mktemp, sed, zenity
 
-set -e
+# ERROR HANDLING    {{{1
 
-# default download directory
-DOWNLOAD_DIR=${DOWNLOAD_DIR:-$QUTE_DOWNLOAD_DIR}
-DOWNLOAD_DIR=${DOWNLOAD_DIR:-$HOME/Downloads}
-# save-as dialog
-ZENITY=${ZENITY:-zenity}
-# html2text command
-HTML2TEXT=${HTML2TEXT:-html2text}
-# sed command
-SED="${SED:-sed}"
-# mktemp command
-MKTEMP="${MKTEMP:-mktemp}"
-# rm command
-RM="${RM:-rm}"
-# cp command
-CP="${CP:-cp}"
-# default file path
-FILE_NAME="$(basename "$QUTE_URL")"
-FILE_NAME_MD="${FILE_NAME%.*}.md"
-DEFAULT_PATH="$DOWNLOAD_DIR/$FILE_NAME_MD"
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+#set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
 
-# subroutines
+# VARIABLES    {{{1
+
+self="$(basename "$0")"
+# required tools findable on system path
+required_system_tools=(basename cp getopt pandoc mktemp sed zenity)
+# download directory
+download_dir=${download_dir:-$QUTE_DOWNLOAD_DIR}
+download_dir=${download_dir:-$HOME/Downloads}
+# filename
+file_name="$(basename "$QUTE_URL")"
+file_name_md="${file_name%.*}.md"
+# default save filepath
+default_path="$download_dir/$file_name_md"
+# }}}1
+
+# PROCEDURES
+
+# checkPrereqs()    {{{1
+#   intent: check for required tools
+#   params: nil
+#   prints: error message if tool(s) missing
+#   return: n/a, aborts scipts on failure
+function checkPrereqs() {
+	local missing tool
+	missing=()
+	# these tools can be found on the base system path
+	for tool in "${required_system_tools[@]}"; do
+		command -v "$tool" &>/dev/null || missing+=("$tool")
+	done
+	if [[ ${#missing[@]} -ne 0 ]]; then
+		local msg
+		msg="Can't run without: $(joinBy ', ' "${missing[@]}")"
+		echo "$self: $msg" >/dev/stderr
+		# cannot use 'log' function here:
+		# - options have not yet been processed
+		logger --priority "user.err" --tag "$self" "$msg"
+		exit 1
+	fi
+	unset required_system_tools required_local_tools
+}
+# displayUsage()    {{{1
+#   intent: display usage information
+#   params: nil
+#   prints: nil
+#   return: nil
+displayUsage() {
+	cat <<_USAGE
+${self}: Qutebrowser userscript to save page as markdown file
+
+Qutebrowser userscript that converts the current page to
+markdown and saves it to a filepath specified by the user.
+
+Not intended to be run from the command line.
+_USAGE
+}
+# processOptions([@options])    {{{1
+#   intent: process all command line options
+#   params: @options - all command line parameters
+#   prints: feedback
+#   return: nil
+#   note:   after execution variable @ARGS contains
+#           remaining command line args (after options removed)
+processOptions() {
+	# read the command line options
+	local OPTIONS
+	if ! OPTIONS="$(
+		getopt \
+			--options hvd \
+			--long help,verbose,debug \
+			--name "${BASH_SOURCE[0]}" \
+			-- "${@}"
+	)"; then
+		# getopt displays errors
+		exit 1
+	fi
+	eval set -- "${OPTIONS}"
+	while true; do
+		case "${1}" in
+		-h | --help)
+			displayUsage
+			exit 0
+			;;
+		-v | --verbose)
+			set -o verbose
+			shift 1
+			;;
+		-d | --debug)
+			set -o xtrace
+			shift 1
+			;;
+		--)
+			shift
+			break
+			;;
+		*) break ;;
+		esac
+	done
+}
+# joinBy($delim, @items)    {{{1
+#   intent: join all items using delimiter
+#   params: delim - delimiter
+#           items - items to be joined
+#   prints: string containing joined items
+#   return: nil
+function joinBy() {
+	local delimiter first_item
+	delimiter="${1:-}"
+	shift
+	first_item="${1:-}"
+	shift
+	printf %b%s "$first_item" "${@/#/$delimiter}"
+}
+# msg($type, $message)    {{{1
+#   intent: display message
+#   params: type    - messasge type [string, required, must be {info,error}]
+#           message - message to display [string, required]
+#   prints: nil
+#   return: nil
 msg() {
-    # msg TYPE MESSAGE
-    # - display message MSG of type TYPE ('info' or 'error')
-    local cmd="$1" ; shift ; local msg="$*"
-    echo "message-$cmd '${msg//\'/\\\'}'" >> "$QUTE_FIFO"
+	local cmd="$1"
+	shift
+	local msg="$*"
+	echo "message-$cmd '${msg//\'/\\\'}'" >>"$QUTE_FIFO"
 }
+# info($message)    {{{1
+#   intent: display informational message
+#   params: message - informational message to display [string, required]
+#   prints: nil
+#   return: nil
 info() {
-    # info MSG
-    # - display message MSG
-    msg info "$*"
+	msg info "$*"
 }
+# die($message)    {{{1
+#   intent: display error message and exit
+#   params: message - error message to display [string, required]
+#   prints: nil
+#   return: nil
 die() {
-    # die MSG
-    # - display error message MSG and exit
-    msg error "$*"
-    # the above error message already informs the user about the failure;
-    # no additional "userscript exited with status 1" is needed.
-    exit 0
+	msg error "$*"
+	# the above error message already informs the user about the failure;
+	# no additional "userscript exited with status 1" is needed.
+	exit 0
 }
-escape() {
-    # escape FILE CHAR
-    # - throughout FILE escape character CHAR with backslash
-    local file="$1" char="$2"
-    $SED -i -e "s/$char/\\\\$char/g" "$file" || true
-}
-replace() {
-    # replace FILE TARGET REPLACE
-    # - throughout FILE replace string TARGET with string REPLACE
-    local file="$1" target="$2" replace="$3"
-    $SED -i -e "s/$target/$replace/g" "$file" || true
-}
-required() {
-    # required CMD [CMD2 ...]
-    # - check that CMDs are available; die if any are not
-    local -a cmds
-    while [ $# -gt 0 ] ; do cmds=("${cmds[@]}" "$1") ; shift ; done
-    for cmd in "${cmds[@]}" ; do
-        if ! which "$cmd" > /dev/null ; then
-            die "Command not found in PATH: ${cmd}"
-        fi
-    done
-}
+# }}}1
 
-# requirements
-[ -d "$DOWNLOAD_DIR" ] || die "Download directory not found: $DOWNLOAD_DIR"
-required "$ZENITY" "$HTML2TEXT" "$SED" "$MKTEMP" "$RM" "$CP"
+# MAIN
 
-# get download file path
-md_path="$( \
-    $ZENITY \
-        --title 'Save as...' \
-        --file-selection \
-            --save \
-            --filename="$DEFAULT_PATH" \
-            --confirm-overwrite \
-            --file-filter="*.md" \
-    )" || true
+# check for required tools    {{{1
+checkPrereqs
+
+# get download file path    {{{1
+# - zenity automatically confirms overwriting with user
+#   if save file already exists
+md_path="$(
+	zenity \
+		--title 'Save as...' \
+		--file-selection \
+		--save \
+		--filename="$default_path" \
+		--file-filter="*.md"
+)" || true
 [ -n "$md_path" ] || die 'No download file path set'
 
-# convert html file to temporary markdown file
-temp_file="$($MKTEMP --tmpdir qutebrowser_XXXXXXXX.md)"
-$HTML2TEXT -utf8 -width 1000000 "$QUTE_HTML" \
-    | $SED -e '/.*/G' \
-    > "$temp_file" \
-    || true
+# convert html file to temporary markdown file    {{{1
+temp_file="$(mktemp --tmpdir qutebrowser_XXXXXXXX.md)"
+trap '[[ -f "$temp_file" ]] && rm -rf "$temp_file"' EXIT
+pandoc --from=html --to=markdown --output="$temp_file" "$QUTE_URL" || true
 [ -f "$temp_file" ] || die 'Unable to generate markdown output file'
 [ -s "$temp_file" ] || die 'Markdown output file is empty'
 
-# escape and replace special characters
-escape "$temp_file" '_' || true
-escape "$temp_file" '*' || true
-replace "$temp_file" '“' '"' || true
-replace "$temp_file" '”' '"' || true
-replace "$temp_file" "’" "'" || true
-replace "$temp_file" "‘" "'" || true
-replace "$temp_file" "‒" "--" || true
-replace "$temp_file" "–" "--" || true
-replace "$temp_file" "—" "--" || true
-replace "$temp_file" "…" "..." || true
-
-# copy temp file to output file location
-if [ -f "$md_path" ];  then
-    $RM "$md_path" || true
-    [ -f "${md_path}" ] && die "Unable to overwrite existing file ${md_path}"
+# copy temp file to output file location    {{{1
+if [[ -f "$md_path" ]]; then
+	rm "$md_path" || true
+	[[ -f "${md_path}" ]] && die "Unable to overwrite existing file ${md_path}"
 fi
-$CP "$temp_file" "$md_path" || true
-[ -f "${md_path}" ] || die "Unable to save $md_path"
+cp "$temp_file" "$md_path" || true
+[[ -f "${md_path}" ]] || die "Unable to save $md_path"
 
-# if here then must have succeeded
+# final feedback    {{{1
+# - if here then must have succeeded
 info "Saved $md_path"
+# }}}1
+
+# vim:foldmethod=marker:
