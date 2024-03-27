3f7f64d327a4a5f9905d152e9ef13711a67852bd
David Nebauer <david@nebauer.org>
2024-03-27 17:05:47 +0930

    add note explaining why plugin manually loaded

--- vim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ vim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -1,5 +1,7 @@
 --[[ nvim-treesitter/nvim-treesitter-context : lightweight alternative to context.vim ]]
 
+-- automatically loaded since lazyvim 10.x, but load here to disable mappings
+
 return {
   "nvim-treesitter/nvim-treesitter-context",
   -- disable all plugin key mappings
