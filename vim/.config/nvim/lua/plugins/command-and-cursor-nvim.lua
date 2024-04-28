bc77b9d8a51d787f1f3fe47f7c39c847328ab75e
David Nebauer <david@nebauer.org>
2024-04-29 06:46:36 +0930

    add plugin: moyiz/command-and-cursor.nvim

---
+++ vim/.config/nvim/lua/plugins/command-and-cursor-nvim.lua
@@ -0,0 +1,7 @@
+--[[ moyiz/command-and-cursor.nvim : highlight cursor and visual selections when entering command mode ]]
+
+return {
+  "moyiz/command-and-cursor.nvim",
+  event = "VeryLazy",
+  opts = {},
+}
