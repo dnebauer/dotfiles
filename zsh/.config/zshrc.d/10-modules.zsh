0e8c05babdff816597f4dd058091d79d496bafca
David Nebauer <david@nebauer.org>
2024-07-18 21:01:02 +0930

    remove support for vim deoplete plugin

--- zsh/.config/zshrc.d/10-modules.zsh
+++ zsh/.config/zshrc.d/10-modules.zsh
@@ -1,6 +1,5 @@
 # Modules    {{{1
-# zpty used by vim plugin zchee/deoplete-zsh
 # zcomplist provides menuselect keymap
-zmodload zsh/files zsh/zutil zsh/complist zsh/zpty
+zmodload zsh/files zsh/zutil zsh/complist
 
 
